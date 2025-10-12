818a707741af2593598953f8510a2b4c05b5cdea
Vytautas Vilkas <vytautas.vilkas@kickertech.com>
2025-10-12 18:09:39 +0300

    remake footer

--- my-cv-page/src/components/Footer.jsx
+++ my-cv-page/src/components/Footer.jsx
@@ -1,22 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
-    <footer
-      className="relative w-full text-[#4a3f35] font-oldStandard py-4 px-6 mt-auto shadow-inner"
-      style={{ minHeight: '100px' }}
-    >
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start pb-2">
-        <div className="text-sm text-left">
-          <p className="font-bold">Contacts:</p>
-            <>
-              <p>Phone: +37060600856</p>
-              <p>Email: Vytautas@Vilkas.lt</p>
-            </>
-        </div>
-      </div>
-      <div className="border-t border-[#e0ded8] mt-2"></div>
-      <div className="text-sm text-center pt-2">
+    <footer className="w-full bg-black text-green-400 font-mono border-t border-green-500/20 mt-auto">
+      <div className="max-w-6xl mx-auto px-6 py-4 text-xs text-green-500/70 text-center">
         © {new Date().getFullYear()} All rights reserved
       </div>
     </footer>
@@ -24,3 +11,4 @@ function Footer() {
 }
 
 export default Footer;
+
